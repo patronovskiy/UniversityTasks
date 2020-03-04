e1a851ea8bbb9dd0fad384b656b405b137bb1e53
Galina Martiushova <patronovskiy@mail.ru>
2020-03-04 19:45:55 +0500

    Lab: add Task3 in Lab 1 C#

---
+++ Semester_02/Lab_01_C_sharp/Task3/Task3/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Overrided obj1 = new Overrided('e');
+            Overrided obj2 = new Overrided('r');
+            Console.Write("Object1. ");
+            obj1.show();
+            Console.Write("Object2. ");
+            obj2.show();
+
+            string text = obj1 + obj2;
+            Console.WriteLine("Object1 + Object2 = " + text);
+
+            int diff = obj1 - obj2;
+            Console.WriteLine("Object1 - Object2 = " + diff);
+
+        }
+    }
+
+    public class Overrided
+    {
+        private char symbol;
+
+        public Overrided(char symbol)
+        {
+            this.symbol = symbol;
+        }
+
+        public void show()
+        {
+            Console.WriteLine("Value of char field: " + this.symbol);
+        }
+
+        public static string operator + (Overrided obj1, Overrided obj2)
+        {
+            string text = "" + obj1.symbol + obj2.symbol;
+            return text;
+        }
+
+        public static int operator - (Overrided obj1, Overrided obj2)
+        {
+            int diff = obj1.symbol - obj2.symbol;
+            return diff;
+        }
+
+        //versions of operators for += using and -= using
+        /*
+        public static Overrided operator + (Overrided obj1, Overrided obj2)
+        {
+            Overrided res = new Overrided((char) (obj1.symbol + 1));
+            return res;
+        }
+
+        public static Overrided operator - (Overrided obj1, Overrided obj2)
+        {
+            Overrided res = new Overrided((char)(obj2.symbol - 1));
+            return res;
+        }
+        */
+
+    }
+}
