dbffa0d55e1618ecd8442fd477d40bb98073443a
Galina Martiushova <patronovskiy@mail.ru>
2020-03-05 22:29:46 +0500

    lab: add Task6 in Lab01 C#

---
+++ Semester_02/Lab_01_C_sharp/Task6/Task6/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextArray obj = new TextArray("И", "в", "том", "краю", "в", "лихом", "бою", "мы", "будем", "петь", "печаль", "свою");
+            obj.show();
+
+            Console.WriteLine("Get element # 9: " + obj[9] +"\n");
+            Console.WriteLine("Get element # 15: " + obj[15] + "\n");
+            Console.WriteLine("Set obj[11] = \"твою\"");
+            obj[11] = "твою";
+            obj.show();
+
+            Console.WriteLine("Get element # 5: " + obj[5] + "\n");
+            Console.WriteLine("Get obj[5, 2]: " + obj[5, 2] + "\n");
+            Console.WriteLine("Get obj[5, 6]: " + obj[5, 10] + "\n");
+
+
+        }
+    }
+
+    class TextArray
+    {
+        private string[] array;
+
+        public TextArray(params string[] texts)
+        {
+            array = texts;
+        }
+
+        public string this[int index]
+        {
+            get
+            {
+                index = index % this.array.Length;
+                return this.array[index];
+            }
+            set
+            {
+                index = index % this.array.Length;
+                this.array[index] = value;
+            }
+        }
+
+        public char this[int index, int charIndex]
+        {
+            get
+            {
+                charIndex = charIndex % array[index].Length;
+                char[] chars = array[index].ToCharArray();
+                return chars[charIndex];
+            }
+        }
+
+        public void show()
+        {
+            Console.WriteLine("Text array (length " + this.array.Length +") :");
+            int counter = 0;
+            foreach (string text in array)
+            {
+                if (counter < 6)
+                {
+                    Console.Write(text + " ");
+                    counter++;
+                } else
+                {
+                    Console.WriteLine(text);
+                    counter = 0;   
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+    }
+}
