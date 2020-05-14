c65d6f0c844ee6a63c8e4e689008412ed4bdd1b2
Galina Martiushova (patronovskiy) <patronovskiy@mail.ru>
2020-05-14 22:02:02 +0500

    Parser: add class for handling punctuation

--- Semester_02/Parser/src/logic/WordsHandler.java
+++ Semester_02/Parser/src/logic/WordsHandler.java
@@ -1,4 +1,35 @@
 package logic;
 
 public class WordsHandler {
+    //class for handling of punctuation
+    public static char[] marks = {',', '.', ':', ';', '?', '/', '!', '#', '*', '(', ')', '{', '}', '[', ']', '<', '>', '&', '-'};
+
+    //method for deleting punctuation marks
+    public static String handle(String initialWord) {
+        String word = initialWord;
+        //case when the whole word is a mark (for example, -)
+        if (word.length() == 1) {
+            for(char mark : marks) {
+                if(word.charAt(0) == mark) {
+                    return word;
+                }
+            }
+        }
+
+        boolean isAllMarksDeleted = false;
+        while(!isAllMarksDeleted) {
+            for (int i = 0; i < marks.length; i++) {
+                if (word.charAt(0) == marks[i]) {
+                    word = word.substring(1, word.length());
+                    isAllMarksDeleted = false;
+                } else if (word.charAt(word.length()-1) == marks[i]) {
+                    word = word.substring(0, word.length()-1);
+                    isAllMarksDeleted = false;
+                } else {
+                    isAllMarksDeleted = true;
+                }
+            }
+        }
+        return word;
+    }
 }
