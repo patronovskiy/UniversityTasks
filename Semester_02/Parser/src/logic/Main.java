58eb075614fb95f78b04e818dc8d5a1df6a63ae1
Galina Martiushova (patronovskiy) <patronovskiy@mail.ru>
2020-05-14 22:03:03 +0500

    Parser: add method for handling title tag

--- Semester_02/Parser/src/logic/Main.java
+++ Semester_02/Parser/src/logic/Main.java
@@ -1,18 +1,18 @@
-package java;
+package logic;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        String address = "https://jsoup.org/cookbook/extracting-data/working-with-urls";
+        String address = "https://seo-akademiya.com/baza-znanij/vnutrennyaya-optimizacziya/atributyi-alt-i-title/";
+        String[] keys = {"alt", "SeO", "aaaa"};
         try {
             Parser parser = new Parser();
             parser.parseHTML(address, true);
-            parser.checkTitle();
+            parser.checkTitle(keys);
         } catch (IOException e) {
             System.out.println("IO err 1: " + e);
         }
-
 
     }
 }
